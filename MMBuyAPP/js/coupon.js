e2689dd71b9884575cba9df37ffe3829bb17ebbb
xieyanlan <1015221051@qq.com>
2018-01-19 00:22:34 +0800

    慢慢买练习

---
+++ MMBuyAPP/js/coupon.js
@@ -0,0 +1,17 @@
+/*
+* @Author:  xyl
+* @Date:   2016-10-25 20:52:09
+* @Last Modified by:   xyl
+* @Last Modified time: 2016-10-27 16:58:01
+*/
+
+'use strict';
+$(function (){
+	setCoupon($('.coupon-title'))
+	function setCoupon(dom,callback){
+		$.ajax({url:"http://mmb.ittun.com/api/getcoupon",success:function (data) {
+			var html = template('couponTitle',data);
+			dom.html(html);
+		}})
+	}
+});
---
+++ MMBuyAPP/js/discount.js
@@ -0,0 +1,22 @@
+/*
+ * @Author:  xyl
+ * @Date:   2016-10-24 22:14:54
+ * @Last Modified by:    xyl
+ * @Last Modified time: 2016-10-26 15:17:32
+ */
+
+'use strict';
+$(function() {
+    setProductList($('.discount-product'), $.getUrlParam('productid'))
+
+    function setProductList(dom, productid, callback) {        
+        $.ajax({
+            url: "http://mmb.ittun.com/api/getdiscountproduct",
+            data: { 'productid': productid },
+            success: function(data) {
+                var html = template("discountProduct",data);
+                dom.html(html);
+            }
+        })
+    }
+});
